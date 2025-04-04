1ba81803e9fb528fb28768f610b2dbfb71c69a83
shahinalom <shahinalom3511371@gmail.com>
2025-04-04 20:06:06 +0600

    error solve

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 app.use(express.json());
-require('dotenv').config()
 const jwt = require("jsonwebtoken");
+require('dotenv').config()
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+
+
 const port = process.env.PORT || 5000;
 
 
@@ -31,6 +34,7 @@ async function run() {
             const userCollection = client.db('edublink').collection('users')
             const courseCollection = client.db('edublink').collection('course')
             const addTeacherCollection = client.db('edublink').collection('teacher')
+            const paymentsCollection = client.db('edublink').collection('payments')
 
             // JWT Token
             app.post('/jwt', async (req, res) => {
@@ -198,6 +202,43 @@ async function run() {
                   res.send(result)
             })
 
+
+
+            // Payment Related Data
+            app.post("/create-payment-intent", async (req, res) => {
+                  const { price } = req.body;
+                  const amount = parseInt(price * 100);
+                  console.log('amount', amount)
+                  try {
+                        const paymentIntent = await stripe.paymentIntents.create({
+                              amount: amount,
+                              currency: "usd",
+                              payment_method_types: ["card"],
+                        });
+                        res.send({
+                              clientSecret: paymentIntent.client_secret,
+                        });
+                  } catch (error) {
+                        res.status(500).send({ error: error.message });
+                  }
+
+            });
+
+
+
+            // Check if the user already paid
+            // app.get('/payments/check/:email', async (req, res) => {
+            //       const { email } = req.params;
+            //       try {
+            //             const existingPayment = await Payment.findOne({ email });
+            //             if (existingPayment) {
+            //                   return res.json({ paid: true });
+            //             }
+            //             res.json({ paid: false });
+            //       } catch (err) {
+            //             res.status(500).json({ error: err.message });
+            //       }
+            // });
 
 
 
