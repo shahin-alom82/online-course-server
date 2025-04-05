db797cb194688361c2e7be4da679803bec9e1fca
shahinalom <shahinalom3511371@gmail.com>
2025-04-06 00:11:59 +0600

    update

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 app.use(
       cors({
             origin: [
-                  "http://localhost:5173",
+                  "https://edublink.vercel.app",
                   "https://edublinkserver.vercel.app",
             ],
             credentials: true,
