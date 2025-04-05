49c52ba69ab39a5c99494d10de19d10bd39b9732
shahinalom <shahinalom3511371@gmail.com>
2025-04-06 00:01:11 +0600

    update

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(
       cors({
             origin: [
                   "http://localhost:5173",
-                  "http://localhost:5000",
+                  "https://edublinkserver.vercel.app",
             ],
             credentials: true,
       })
